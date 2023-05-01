6c89200304a0aa9c0b5b6516f99d261d94765bb1
rscaldo <rscaldo@hotmail.com>
2023-05-01 06:18:00 -0300

    criando o hero e importando a font

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
 
 function styles() {
   return gulp
@@ -7,7 +8,16 @@ function styles() {
     .pipe(sass({ outputStyles: "compressed" }))
     .pipe(gulp.dest("./dist/css"));
 }
-exports.default = styles;
+
+function images() {
+  return gulp
+    .src("./src/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/images"));
+}
+
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function () {
   gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
 };
